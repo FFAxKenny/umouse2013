6caf4b838dfeb8e6139d58c7f784fafa2375af65
Bronson Oka <abunai59@gmail.com>
2013-04-19 20:05:31 -1000

    Include macros for motor sequencing
    x = (x & 0b1111110000111111) + 0b0000000001000000);
    AND then ADD for new value

--- Motors.h
+++ Motors.h
@@ -8,3 +8,18 @@
 #define STOP 	 5
 #define TURN     6
 
+// Values to AND for left and right sequence
+#define LS_AND	0b1100001111111111
+#define RS_AND	0b1111110000111111
+
+// Values to ADD for the left sequence
+#define LS_0	0b0000010000000000
+#define LS_1	0b0000100000000000
+#define LS_2	0b0001000000000000
+#define LS_3	0b0010000000000000
+
+// Values to ADD for the right sequence
+#define RS_0	0b0000001000000000
+#define RS_1	0b0000000100000000
+#define RS_2	0b0000000010000000
+#define RS_3	0b0000000001000000
