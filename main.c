bd773b92494bfa00fe2005e343f5889da5e9448c
Bronson Oka <abunai59@gmail.com>
2013-04-24 10:09:50 -1000

    Carry out ADC in main

--- main.c
+++ main.c
@@ -9,9 +9,24 @@
 /* End Interrupt Definitions */
 int main(void)
 {
+    int ADCValue = 0;
+
 	// initialize the mouse
 	init_all();
 
-    while (1){}
+    while (1){
+
+        //Convert
+
+        AD1CON1bits.SAMP = 1;           // Start Conversion
+        ADC1BUF0 = 0x0000;              // Clear ADC1 Buffer
+        while (!AD1CON1bits.DONE);      // Conversion done? Waiting for 1
+        AD1CON1bits.DONE = 0;           // Clear conversion done status bit
+        ADCValue = ADC1BUF0;
+
+        if(ADCValue > 2000) DIR = STOP;
+        else DIR = TURN;
+
+    }
 }
 
