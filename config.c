d6348876b408ce9e144d0a760c0fa9fe2bedd630
Bronson Oka <abunai59@gmail.com>
2013-04-25 09:01:14 -1000

    Adjust initialization to get rid of drift

--- config.c
+++ config.c
@@ -27,14 +27,14 @@ void init_all(void) {
 	// initialize clock
 	init_clock();
 
-	// configure interrupts
-	config_interrupts();
-
 	// configure I/O
 	config_io();
 
     // initialize ADC
     ADC_Init();
+
+	// configure interrupts
+	config_interrupts();
 }
 
 // initialize global variables
@@ -68,74 +68,6 @@ void init_globals(void) {
 	DIR = FORWARD;
 }
 
-// configure interrupts
-void config_interrupts(void) {
-    //Set interrupt
-    //	INTCON = 0xC0; //Enable Global,Timer0 Overflow interrupt
-    INTCON1bits.NSTDIS = 1;  // disable nested interrupts
-    // set 16 bit
-    //	T1CONbits.RD16 = 1;
-    // set up timer 1 prescaler for 1:2
-    T1CONbits.TCKPS1 = 0;
-    T1CONbits.TCKPS0 = 1;
-    IPC0bits.T1IP2 = 1; // TMR 1 high priority
-    IPC0bits.T1IP1 = 1; // TMR 1 high priority
-    IPC0bits.T1IP0 = 1; // TMR 1 high priority
-    IEC0bits.T1IE = 1; // enable the timer 1 interrupt
-    IFS0bits.T1IF = 0; // turn off TMR1 flag
-    T1CONbits.TCS = 0; // internal clock
-    PR1=PR1_MAX;  // max speed using prescale 01
-
-    // set 16 bit
-    //	T2CONbits.RD16 = 1;
-    // set up timer 2 prescaler for 1:2
-    T2CONbits.TCKPS1 = 0;
-    T2CONbits.TCKPS0 = 1;
-    IPC1bits.T2IP2 = 1; // TMR 1 high priority
-    IPC1bits.T2IP1 = 1; // TMR 1 high priority
-    IPC1bits.T2IP0 = 1; // TMR 1 high priority
-    IEC0bits.T2IE = 1; // enable the timer 1 interrupt
-    IFS0bits.T2IF = 0; // turn off TMR1 flag
-    T2CONbits.TCS = 0; // internal clock
-    PR2=PR2_MAX;  // max speed using prescale 01
-	
-	// set 16 bit
-    // set up timer 4 prescaler for 1:256
-    T4CONbits.TCKPS1 = 1;
-    T4CONbits.TCKPS0 = 1;
-    IPC6bits.T4IP2 = 1; // TMR 4 high priority
-    IPC6bits.T4IP1 = 1; // TMR 4 high priority
-    IPC6bits.T4IP0 = 1; // TMR 4 high priority
-    IEC1bits.T4IE = 1; // enable the timer 4 interrupt
-    IFS1bits.T4IF = 0; // turn off TMR4 flag
-    T4CONbits.TCS = 0; // internal clock
-
-	// start timers
-
-	T4CONbits.TON = 1; 	// turn on timer 4
-	while(!RDY){}		// use timer 4 to delay startup
-	
-	// Turn on timers
-    T1CONbits.TON = 1; // turn on timer 1
-    T2CONbits.TON = 1; // turn on timer 2
-	T4CONbits.TON = 0; // turn off timer 4
-
-}
-
-// wait to start the timers
-// Use timer 4 for delay/counting
-void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
-{
-    DELAY_COUNT ++;
-	if(DELAY_COUNT == 5) { // delays 2.5s
-		RDY = 1;
-	}
-
-    IFS1bits.T4IF = 0; // turn off TMR2 flag
-   // PR4=PR4_MAX;  // max speed using prescale 01
-	
-}
-
 // configure clock
 void init_clock(void) {
     // In order to configure the device to operate at 40 MHz, configure the PLL prescaler, 
@@ -157,4 +89,79 @@ void config_io(void) {
 	TRISA=0b11;
 	TRISB=0x0003;
     AD1PCFGL = 0xFFFF;
+}
+
+// configure interrupts
+void config_interrupts(void) {
+    //Set interrupt
+    //	INTCON = 0xC0; //Enable Global,Timer0 Overflow interrupt
+    INTCON1bits.NSTDIS = 1;  // disable nested interrupts
+    // set 16 bit
+    //	T1CONbits.RD16 = 1;
+    // set up timer 1 prescaler for 1:2
+    T1CONbits.TCKPS1 = 0;
+    T1CONbits.TCKPS0 = 1;
+    IPC0bits.T1IP2 = 1; // TMR 1 high priority
+    IPC0bits.T1IP1 = 1; // TMR 1 high priority
+    IPC0bits.T1IP0 = 1; // TMR 1 high priority
+    IEC0bits.T1IE = 1; // enable the timer 1 interrupt
+    IFS0bits.T1IF = 0; // turn off TMR1 flag
+    T1CONbits.TCS = 0; // internal clock
+    PR1=PR1_INIT;  // max speed using prescale 01
+
+    // set 16 bit
+    //	T2CONbits.RD16 = 1;
+    // set up timer 2 prescaler for 1:2
+    T2CONbits.TCKPS1 = 0;
+    T2CONbits.TCKPS0 = 1;
+    IPC1bits.T2IP2 = 1; // TMR 1 high priority
+    IPC1bits.T2IP1 = 1; // TMR 1 high priority
+    IPC1bits.T2IP0 = 1; // TMR 1 high priority
+    IEC0bits.T2IE = 1; // enable the timer 1 interrupt
+    IFS0bits.T2IF = 0; // turn off TMR1 flag
+    T2CONbits.TCS = 0; // internal clock
+    PR2=PR2_INIT;  // max speed using prescale 01
+	
+	// set 16 bit
+    // set up timer 4 prescaler for 1:256
+    T4CONbits.TCKPS1 = 1;
+    T4CONbits.TCKPS0 = 1;
+    IPC6bits.T4IP2 = 1; // TMR 4 high priority
+    IPC6bits.T4IP1 = 1; // TMR 4 high priority
+    IPC6bits.T4IP0 = 1; // TMR 4 high priority
+    IEC1bits.T4IE = 1; // enable the timer 4 interrupt
+    IFS1bits.T4IF = 0; // turn off TMR4 flag
+    T4CONbits.TCS = 0; // internal clock
+
+	// start timers
+    T1CONbits.TON = 1; // turn on timer 1
+    T2CONbits.TON = 1; // turn on timer 2
+	T4CONbits.TON = 1; 	// turn on timer 4
+
+	// prevent inital drift
+	DIR = FORWARD;
+	while(!RDY){}		// use timer 4 to delay startup
+	DIR = STOP;
+    PR1=PR1_MAX;  // max speed using prescale 01
+    PR2=PR2_MAX;  // max speed using prescale 01
+
+	// Turn off timer 4
+	T4CONbits.TON = 0; // turn off timer 4
+
+	// Wait for go signal
+	
+}
+
+// wait to start the timers
+// Use timer 4 for delay/counting
+void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
+{
+    DELAY_COUNT ++;
+	if(DELAY_COUNT == 5) { // delays 2.5s
+		RDY = 1;
+	}
+
+    IFS1bits.T4IF = 0; // turn off TMR2 flag
+   // PR4=PR4_MAX;  // max speed using prescale 01
+	
 }
--- config.h
+++ config.h
@@ -2,10 +2,12 @@
 // for configuration and initialization
 
 // Values for right prescaler
-#define PR1_MAX 0x30FF
+#define PR1_MAX		0x30FF
+#define PR1_INIT	0x8000
 
 // Values for left prescaler
-#define PR2_MAX 0x30FF
+#define PR2_MAX 	0x30FF
+#define PR2_INIT	0x8000
 
 // initialize the mouse
 void init_all(void);
