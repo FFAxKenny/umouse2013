3e51b3ac44a537fb380e0cc74db8170b870154c6
Bronson Oka <abunai59@gmail.com>
2013-04-27 05:50:31 -1000

    Add actual values for walls checking

--- walls.h
+++ walls.h
@@ -4,10 +4,10 @@
 // macros and prototypes for walls.c
 #include <stdbool.h>
 
-#define RF_THRESHOLD	0
-#define LF_THRESHOLD	0
-#define RIGHT_THRESHOLD	0
-#define LEFT_THRESHOLD	0
+#define RF_THRESHOLD	1700
+#define LF_THRESHOLD	1700
+#define RIGHT_THRESHOLD	1700
+#define LEFT_THRESHOLD	1700
 
 bool left_wall(void);
 bool front_wall(void);
