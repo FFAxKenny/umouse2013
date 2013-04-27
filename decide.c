63654ba9a9d83224e0001013ab7bb6578cf23d61
Bronson Oka <abunai59@gmail.com>
2013-04-26 22:38:40 -1000

    Add code for decision making
    Currently implements only right wall hug

---
+++ decide.c
@@ -0,0 +1,15 @@
+// decide.c
+// where should the mouse go next?
+#include <stdbool.h>
+#include "walls.h"
+#include "decide.h"
+
+// all functions should return a turn value
+// L_TURN R_TURN U_TURN or NO_TURN
+int r_wall(void) {
+    // decide while getting walls
+    if (right_wall() == false) return R_TURN;
+    else if (front_wall() == false) return NO_TURN;
+    else if (left_wall() == false) return L_TURN;
+    else return U_TURN;
+}
---
+++ decide.h
@@ -0,0 +1,8 @@
+// protos and macros for decide.c
+#define L_TURN  0
+#define R_TURN  1
+#define F_TURN  2
+#define U_TURN  3
+#define NO_TURN 4
+
+int r_wall();
