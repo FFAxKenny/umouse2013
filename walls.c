6fd69b8379079b6e89aad212359b5029a9398c01
Bronson Oka <abunai59@gmail.com>
2013-04-26 20:30:51 -1000

    Add code for getting walls in boolean form

---
+++ walls.c
@@ -0,0 +1,14 @@
+// functionality for getting binary walls
+#include "walls.h"
+#include "adc.h"
+#include <stdbool.h>
+
+bool left_wall(void) {
+    return (ADC_Sample(L_SENSOR) > LEFT_THRESHOLD);
+}
+bool front_wall(void) {
+    return ((ADC_Sample(RF_SENSOR) > RF_THRESHOLD) && (ADC_Sample(LF_SENSOR) > LF_THRESHOLD));
+}
+bool right_wall(void) {
+    return (ADC_Sample(R_SENSOR) > RIGHT_THRESHOLD);
+}
---
+++ walls.h
@@ -0,0 +1,12 @@
+// macros and prototypes for walls.c
+#include <stdbool.h>
+
+#define RF_THRESHOLD
+#define LF_THRESHOLD
+#define RIGHT_THRESHOLD
+#define LEFT_THRESHOLD
+
+bool left_wall(void);
+bool front_wall(void);
+bool right_wall(void);
+
