79ac285d6a1130a5ca59c474c8ca7eb97a0bf6fa
Bronson Oka <abunai59@gmail.com>
2013-04-26 19:44:37 -1000

    Add comments to global variables

--- main.h
+++ main.h
@@ -1,14 +1,14 @@
 // Declare global variables
 
-extern int L_IND,
-	R_IND,
-	DIR,
-    DELAY_COUNT,
-	MAX_DELAY,
-    RDY,
-    ST_COUNT;
+extern int L_IND,	// Left index
+	R_IND,			// Right index
+	DIR,			// Direction of movement
+    DELAY_COUNT,	// Delay count for delay interrupt
+	MAX_DELAY,		// Max delay for delay interrupt
+    RDY,			// Ready bit for delay interrupt
+    ST_COUNT;		// Step count
 
-extern unsigned int L_SEQ[4];
+extern unsigned int L_SEQ[4];	// Left sequence
 
-extern unsigned int R_SEQ[4];
+extern unsigned int R_SEQ[4];	// Right sequence
 
