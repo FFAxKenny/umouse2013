1914cce0ce65ff4afbcf721b4224e36d632f2165
Bronson Oka <abunai59@gmail.com>
2013-05-01 04:28:09 -1000

    For later
    fix walls to get from flags instead

--- walls.c
+++ walls.c
@@ -4,11 +4,14 @@
 #include <stdbool.h>
 
 bool left_wall(void) {
+    // replace with flags later
     return (ADC_Sample(L_SENSOR) > LEFT_THRESHOLD);
 }
 bool front_wall(void) {
+    // replace with flags later
     return ((ADC_Sample(RF_SENSOR) > RF_THRESHOLD) || (ADC_Sample(LF_SENSOR) > LF_THRESHOLD));
 }
 bool right_wall(void) {
+    // replace with flags later
     return (ADC_Sample(R_SENSOR) > RIGHT_THRESHOLD);
 }
