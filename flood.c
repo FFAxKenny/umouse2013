0cc8690efafb21c630a2c6ddce78d34ac3f5da1b
Bronson Oka <abunai59@gmail.com>
2013-05-02 17:24:28 -1000

    Fix flood
    check through E and handle null cells

--- flood.c
+++ flood.c
@@ -1,5 +1,6 @@
 // floodfill
 #include <stdbool.h>
+#include <stdlib.h>
 #include "maze.h"
 #include "queue.h"
 #include "cell.h"
@@ -70,36 +71,39 @@ void flood_maze(Maze * maze, int start_x, int start_y) {
         // get cell
         cur_cell = q_pop(q);
 
-        // get cell value
-        old_val = get_val(cur_cell);
-        // get position
-        x = get_x(cur_cell);
-        y = get_y(cur_cell);
+		// don't process a null cell
+		if (cur_cell != NULL) {
+	        // get cell value
+	        old_val = get_val(cur_cell);
+    	    // get position
+    	    x = get_x(cur_cell);
+			y = get_y(cur_cell);
 
-        // initialize new cell value
-        new_val = old_val;
+	        // initialize new cell value
+	        new_val = old_val;
 
-        // check accessible neighbors
-        for (dir = N ; dir < E ; dir++) {
-            if(has_wall_dir(maze,x,y,dir) == false) {
-                temp_val = get_val_dir(maze,x,y,dir);
-                // if accessed value is less than new value
-                // new value = accessed + 1
-                if(temp_val < new_val) new_val = temp_val + 1;
-            }
-        }
+	        // check accessible neighbors
+	        for (dir = N ; dir <= E ; dir++) {
+	            if(has_wall_dir(maze,x,y,dir) == false) {
+	                temp_val = get_val_dir(maze,x,y,dir);
+	                // if accessed value is less than new value
+	                // new value = accessed + 1
+	                if(temp_val < new_val) new_val = temp_val + 1;
+	            }
+	        }
 
-        // if new value is different
-        // update value and add all accessible neighbors to queue
-        // otherwise, do nothing
-        if(old_val != new_val) {
-            set_val(cur_cell, new_val);
-            for (dir = N ; dir < E ; dir++) {
-                if(has_wall_dir(maze,x,y,dir) == false) {
-                    q_push(q, get_cell_dir(maze,x,y,dir));
-                }
-            }
-        }
+	        // if new value is different
+	        // update value and add all accessible neighbors to queue
+	        // otherwise, do nothing
+	        if(old_val != new_val) {
+	            set_val(cur_cell, new_val);
+	            for (dir = N ; dir <= E ; dir++) {
+	                if(has_wall_dir(maze,x,y,dir) == false) {
+	                    q_push(q, get_cell_dir(maze,x,y,dir));
+	                }
+	            }
+	        }
+		}
     }
 }
 
